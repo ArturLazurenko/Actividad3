0022138150f8e53de8019c32e7cca91960c7aaaf
A <arturlazurenko1@gmail.com>
2026-02-04 11:41:20 -0600

    V1.1

---
+++ Actividades.java
@@ -0,0 +1,21 @@
+package Actividad3;
+
+import java.util.Scanner;
+
+public class Actividades {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] calificaciones;
+
+        
+
+        
+        System.out.println("Ingrese cuantas calificaciones quiere poner: ");
+        int clases = sc.nextInt();
+
+
+
+        sc.close();
+    }
+}
